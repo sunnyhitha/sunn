949b42f5c9f9fbf054bf3a4d083cdcb0ee553705
sunnyhitha <41725679+sunnyhitha@users.noreply.github.com>
2018-07-26 23:14:52 +0530

    Update sunn.java

--- sunn.java
+++ sunn.java
@@ -7,7 +7,7 @@ class Vowel
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		 char ch = 'f';
+		 char ch = 'e';
 		 {
 		     if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
                                     System.out.println("Vowel");
